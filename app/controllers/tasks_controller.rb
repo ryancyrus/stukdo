c2f1f8f21af7bf17633d106203ebb9dbd062b92b
Shubhanshu Aarsay <shubh.aarsay@gmail.com>
2015-08-19 21:20:02 +0530

    Added change task custom actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :change]
 
   # GET /tasks
   # GET /tasks.json
@@ -68,6 +68,14 @@ class TasksController < ApplicationController
       format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  def change
+    @task.update_attributes(state: params[:state])
+    respond_to do |format|
+      format.html { redirect_to tasks_path, notice:'Task updated.'}
+      format.json { render :show}
+     end
   end
 
   private
