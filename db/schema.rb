69e5379bd44ea956f3edece195d3d5769ab4a585
Shubhanshu Aarsay <shubh.aarsay@gmail.com>
2015-08-24 00:54:37 +0530

    Added state to our tasks, configured task views

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823185011) do
+ActiveRecord::Schema.define(version: 20150823191115) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
     t.date     "duedate"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.integer  "user_id"
+    t.string   "state",      default: "to_do"
   end
 
   create_table "users", force: :cascade do |t|
